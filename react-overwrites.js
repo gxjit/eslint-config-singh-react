f2497327ae104345538d87973aba393208922fee
Gurjit Singh <g.s@outlook.in>
2019-09-15 19:07:28 +0530

    fixes

--- react-overwrites.js
+++ react-overwrites.js
@@ -2,6 +2,14 @@ module.exports = {
   rules: {
     // Specify whether double or single quotes should be used in JSX attributes
     // https://eslint.org/docs/rules/jsx-quotes
-    'jsx-quotes': ['error', 'prefer-single']
+    'jsx-quotes': ['error', 'prefer-single'],
+
+    // Detect missing key prop (react/jsx-key)
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
+    'react/jsx-key': 'error',
+
+    // Prevent direct mutation of this.state (react/no-direct-mutation-state)
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
+    'react/no-direct-mutation-state': 'error'
   }
 }
